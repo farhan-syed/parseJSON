b09c847e772c7c38e8d02f8c1821b7c9ad150765
Farhan Syed <farhansyed11.3@gmail.com>
2017-04-09 00:55:24 -0700

    Initial Commit

---
+++ parseJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parseJSON
+//
+//  Created by Farhan Syed on 4/9/17.
+//  Copyright © 2017 Farhan Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
