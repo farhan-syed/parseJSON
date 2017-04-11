b35141e467b341bf8cb0f786d58a649761e6eafb
farhansyed9 <farhansyed11.3@gmail.com>
2017-04-11 12:54:57 -0700

    Changed to parseJSON()

--- parseJSON/ViewController.swift
+++ parseJSON/ViewController.swift
@@ -8,18 +8,91 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController  {
+    
+    var tableArray = [String] ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        
+        parseJSON()
+        
     }
-
+    
+    func parseJSON() {
+        let url = URL(string: "https://api.myjson.com/bins/vi56v")
+        
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            
+            guard error == nil else {
+                print("returning error")
+                return
+            }
+            
+            guard let content = data else {
+                print("not returning data")
+                return
+            }
+            
+            
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+                print("Not containing JSON")
+                return
+            }
+            
+            if let array = json["companies"] as? [String] {
+                self.tableArray = array
+            }
+            
+            print(self.tableArray)
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
 
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        
+        cell.textLabel?.text = self.tableArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.tableArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(self.tableArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 
 }
+
 
